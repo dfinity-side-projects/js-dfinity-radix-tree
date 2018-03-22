f360b75824e9bd1e15f8928424e85e42326fda49
wanderer <mjbecze@gmail.com>
2018-03-22 15:16:17 -0400

    fix link encoding

--- datastore.js
+++ datastore.js
@@ -6,6 +6,13 @@ const cbor = require('borc')
 
 module.exports = class TreeDAG extends DAG {
   async put (val) {
+    if (val[1]) {
+      val[1] = new cbor.Tagged(42, val[1]['/'])
+    }
+
+    if (val[2]) {
+      val[2] = new cbor.Tagged(42, val[2]['/'])
+    }
     const encoded = cbor.encode(val)
     const key = await TreeDAG.getMerkleLink(encoded)
 
@@ -24,6 +31,13 @@ module.exports = class TreeDAG extends DAG {
         } else {
           val = Buffer.from(val, 'hex')
           const decoded = cbor.decode(val)
+          if (decoded[1]) {
+            decoded[1]['/'] = decoded[1].value
+          }
+
+          if (decoded[2]) {
+            decoded[2]['/'] = decoded[2].value
+          }
           resolve(decoded)
         }
       })
